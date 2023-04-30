101b9c92916dedd632b0d5c0f38955f0350c2fe9
dsouths <dsouths@hotmail.com>
2023-04-30 19:37:03 +0000

    created views.py

--- bookingapp/urls.py
+++ bookingapp/urls.py
@@ -1,21 +1,14 @@
-"""bookingapp URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+     path('', views.index, name="index"),
+     path('services/', views.services, name="services"),
+     path('booknow/', views.booknow, name="booknow"),
+     path('bookings/', views.bookings, name='bookings'),
+     path('change/<int:booking_id>/', views.change_booking, name='change_booking'),
+     path('delete-booking/<int:booking_id>/', views.delete_booking, name='delete_booking'),
+    
 ]
