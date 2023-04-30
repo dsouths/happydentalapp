67cdaf799a85e612988f3541de0c997f70481961
dsouths <dsouths@hotmail.com>
2023-04-30 19:39:24 +0000

    added admin.py

--- happydental/admin.py
+++ happydental/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Service, Dentist
+from happydental.models import Booking
 
 # Register your models here.
+
+admin.site.register(Dentist)
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("service_name", "price")
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    """
+    Class registered to represent model in admin database.
+    """
+    list_display = ('user', 'name',
+                    'date',
+                    'time',
+                    'phone',
+                    'email',
+                    'service'
+                    )
+    search_fields = ('user',
+                     'name',
+                     'date',
+                     'phone')
+    list_filter = ('user',
+                   'name',
+                   'date',
+                   'phone')
