230df726a31d28689bd0c133718889fa5c59d2c2
dsouths <dsouths@hotmail.com>
2023-05-01 09:58:24 +0000

    styled navbar & footer

--- bookingapp/urls.py
+++ bookingapp/urls.py
@@ -1,14 +1,8 @@
-
 from django.contrib import admin
-from django.urls import path
-from happydental import views
+from django.urls import path, include
 
 urlpatterns = [
-     path('', views.index, name="index"),
-     path('services/', views.services, name="services"),
-     path('booknow/', views.booknow, name="booknow"),
-     path('bookings/', views.bookings, name='bookings'),
-     path('change/<int:booking_id>/', views.change_booking, name='change_booking'),
-     path('delete-booking/<int:booking_id>/', views.delete_booking, name='delete_booking'),
-    
-]
+    path('admin/', admin.site.urls),
+    path('', include('happydental.urls')),
+    path('accounts/', include('allauth.urls')),
+]    
