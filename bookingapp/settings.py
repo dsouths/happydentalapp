ed9c310ecbaaae29753fbc65dc1fdbcc80fbd283
dsouths <dsouths@hotmail.com>
2023-05-02 17:19:01 +0000

    updated allowed hosts

--- bookingapp/settings.py
+++ bookingapp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["bookingapp.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["happydental.herokuapp.com", "localhost"]
 
 
 # Application definition
