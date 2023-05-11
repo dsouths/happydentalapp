d5101dff5933967190c499949f13f738c0b494c2
dsouths <dsouths@hotmail.com>
2023-05-11 20:32:05 +0000

    checking if css loading on heroku after revert

--- happydental/views.py
+++ happydental/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Booking, Service, Dentist, Appointment
+from .models import *
 from .forms import BookingForm
 import datetime
 from django.contrib import messages
